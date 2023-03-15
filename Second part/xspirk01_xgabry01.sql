55aa0b77098e6cab50b06bd27c1000a74727bd38
Marek Špirka <marek.spirka17@gmail.com>
2023-03-15 17:20:27 +0100

    Added header of second part of project

---
+++ Second part/xspirk01_xgabry01.sql
@@ -0,0 +1,7 @@
+-----------------------------------------------
+---- SQL SCRIPT -> SECOND PART OF PROJECT  ----
+-- Autor: Marek Spirka -- xlogin: xspirk01 ----
+-- Autor: Adam Gabrys  -- xlogin: xgabry01 ----
+-----------------------------------------------
+
+----------------DROP TABLES--------------------
