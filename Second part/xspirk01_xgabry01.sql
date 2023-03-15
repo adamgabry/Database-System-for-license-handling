fdb187f7553a615fd8fb8a132e1217c6c6f50cc9
Marek Špirka <marek.spirka17@gmail.com>
2023-03-15 17:52:33 +0100

    Delete Tables and Basic Make Tables

--- Second part/xspirk01_xgabry01.sql
+++ Second part/xspirk01_xgabry01.sql
@@ -5,3 +5,23 @@
 -----------------------------------------------
 
 ----------------DROP TABLES--------------------
+
+DROP TABLE aplikace;
+DROP TABLE verze;
+DROP TABLE organizace;
+DROP TABLE zamestanec;
+DROP TABLE smlouva;
+DROP TABLE manazer;
+DROP TABLE vyvojar;
+
+----------------MAKE TABLES--------------------
+
+CREATE TABLE aplikace
+(
+    --tu bude ID
+    nazov VARCHAR(255) NOT NULL,
+    popis VARCHAR(255),
+    platforma VARCHAR(255) NOT NULL,
+    webstranka VARCHAR(255) NOT NULL,
+)
+
